3fea6b72f8aa94b84e1ca2356162ff4b474a03d2
Shaun Meyer <meyersh@morningside.edu>
2011-11-12 20:26:32 -0600

    Incorporating Andreas Grupp's German Language pack

---
+++ lang/de/block_quickmail.php
@@ -0,0 +1,55 @@
+<?php
+
+$string['pluginname'] = 'Quickmail';
+$string['quickmail:cansend'] = "Anwendern erlauben Email &uuml;ber Quickmail zu versenden.";
+$string['quickmail:canconfig'] = "Anwendern die Konfiguration einr Quickmail-Instanz erlauben.";
+$string['quickmail:canimpersonate'] = "Anwendern den Login als anderer Anwender und Verlaufsansicht erlauben.";
+$string['composenew'] = 'Neue Email erstellen'; 
+$string['email'] = 'Email';
+$string['drafts'] = 'Entw&uuml;rfe anzeigen';
+$string['history'] = 'Verlauf anzeigen';
+$string['log'] = $string['Verlauf']; 
+$string['from'] = 'Von';
+$string['selected'] = 'Ausgew&auml;hlte Empf&auml;nger';
+$string['add_button'] = 'Hinzuf&uuml;gen';
+$string['remove_button'] = 'Entfernen';
+$string['add_all'] = 'Alles hinzuf&uuml;gen';
+$string['remove_all'] = 'Alles entfernen';
+$string['role_filter'] = 'Rollen Filter';
+$string['no_filter'] = 'Kein Filter';
+$string['potential_users'] = 'M&ouml;gliche Empf&auml;nger';
+$string['potential_sections'] = 'M&ouml;gliche Abschnitte';
+$string['no_section'] = 'In keinem Abschnitt';
+$string['attachment'] = 'Anhang/Anh&auml;nge';
+$string['subject'] = 'Betreff';
+$string['message'] = 'Nachricht';
+$string['send_email'] = 'Email senden';
+$string['save_draft'] = 'Entwurf speichern';
+$string['actions'] = 'Aktionen';
+$string['signature'] = 'Unterschriften';
+$string['delete_confirm'] = 'Sind Sie sich sicher, dass Sie die Mitteilung mit den folgenden Details l&ouml;schen wollen: {$a}';
+$string['title'] = 'Titel';
+$string['sig'] ='Unterschrift';
+$string['default_flag'] = 'Standard';
+$string['config'] = 'Konfiguration';
+$string['receipt'] = 'Eine Kopie empfangen';
+
+// Config form strings
+$string['allowstudents'] = 'Studenten die Verwendung von Quickmail erlauben';
+$string['select_roles'] = 'Rollen filtern nach';
+$string['reset'] = 'Standard-Einstellungen wieder herstellen ';
+
+$string['no_type'] = '{$a} is not in the acceptable type viewer. Please use the applciation correctly.';
+$string['no_email'] = 'Konnte keine Email an {$a->firstname} {$a->lastname} senden.';
+$string['no_log'] = 'Sie haben bislang keinen Email-Verlauf.'; 
+$string['no_drafts'] = 'Sie haben keine Entwurf-Emails.'; 
+$string['no_subject'] = 'Sie m&uuml;ssen einen Betreff haben';
+$string['no_course'] = 'Ung&uuml;ltiger Kurs mit der ID von {$a}';
+$string['no_permission'] = 'Sie haben keine Berechtigung Emails mit Quickmal zu senden.';
+$string['no_users'] = 'Es gibt keine Anwender denen Sie eine Email senden k&ouml;nnen.';
+$string['no_selected'] = 'Sie m&uuml;ssen einige Anwender f&uuml;r das Mailen ausw&auml;hlen.';
+$string['not_valid'] = 'Dies ist kein zul&auml;ssiger Email Log Anzeigetyp: {$a}';
+$string['not_valid_action'] = 'Sie m&uuml;ssen eine g&uuml;ltige Aktion f&uuml;r {$a} angeben:';
+$string['not_valid_typeid'] = 'Sie m&uuml;ssen eine g&uuml;ltige Email f&uuml;r {$a} angeben:';
+$string['delete_failed'] = 'L&ouml;schen der Email fehlgeschlagen';
+$string['required'] = 'Bitte f&uuml;llen Sie die Pflichtfelder aus.';
